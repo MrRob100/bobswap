58861b7259c4810bcfb2be66afa549513a57e787
mrrob100 <robarnold100@hotmail.com>
2022-11-27 19:45:41 -0300

    lists with hover classes and usestates working

--- src/components/list.js
+++ src/components/list.js
@@ -2,6 +2,9 @@ import React, {useEffect, useState} from "react";
 
 const List = (props) => {
 
+    const [token0, setToken0] = useState({id: "", symbol: ""});
+    const [token1, setToken1] = useState({id: "", symbol: ""});
+
     useEffect(() => {
         const fetchData = async() => {
             await props.allPools();
@@ -10,6 +13,14 @@ const List = (props) => {
         fetchData();
 
     }, []);
+
+    useEffect(() => {
+        console.log(token0.symbol, token1.symbol);
+
+        //this method will be in parent
+        //props.checkIfPool(token0.id, token1, id)
+
+    }, [token0, token1]);
 
     const fill = () => {
         console.log('fill');
@@ -21,9 +32,9 @@ const List = (props) => {
                 <div className="h-80 overflow-scroll bg-slate-100">
                     <ul>
                         {props.pools.length > 0 && props.pools.map((item, index) => {
-                            console.log(item);
+                            let active = item.token1.id === token0.id ? 'bg-slate-300' : '';
                             return(
-                                <li onClick={fill} key={index} className="hover:bg-slate-200 cursor-pointer">
+                                <li onClick={() => setToken0(item.token1)} key={index} className={`hover:${active ? active : 'bg-slate-200'} cursor-pointer ${active}`}>
                                     <span>{item.token1.symbol}</span>
                                 </li>
                             )
@@ -33,8 +44,9 @@ const List = (props) => {
                 <div className="h-80 overflow-scroll bg-slate-100">
                     <ul>
                         {props.pools.length > 0 && props.pools.map((item, index) => {
+                            let active = item.token1.id === token1.id ? 'bg-slate-300' : '';
                             return(
-                                <li onClick={fill} key={index} className="hover:bg-slate-200 cursor-pointer">
+                                <li onClick={() => setToken1(item.token1)} key={index} className={`hover:${active ? active : 'bg-slate-200'} cursor-pointer ${active}`}>
                                     <span>{item.token1.symbol}</span>
                                 </li>
                             )
